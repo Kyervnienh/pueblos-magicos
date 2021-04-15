6b92fd7129571791a795a88d1b2506de1f7741f0
raquel-vazquez <lvazquez@novasolutionsystems.com>
2021-04-14 23:18:19 -0500

    feat: configuracion y estructura

---
+++ src/fixtures/dataTownsExample.fixture.js
@@ -0,0 +1,50 @@
+export const dataTows = [
+    {
+        name: 'Town 1',
+        state: 'Estado',
+        infoState: 'Lorem...',
+        img: './assets/',
+        atraccions : [
+            {
+                name: 'Atracción 1',
+                info: 'Lorem...',
+                img: './assets/',
+                cost: '$0',
+                distance: '',
+                type: '',   
+            }
+            {
+                name: 'Atracción 2',
+                info: 'Lorem...',
+                img: './assets/',
+                cost: '$0',
+                distance: '',
+                type: '',   
+            }
+        ]
+    },
+    {
+        name: 'Town 2',
+        state: 'Estado',
+        infoState: 'Lorem...',
+        img: './assets/',
+        atraccions : [
+            {
+                name: 'Atracción 1',
+                info: 'Lorem...',
+                img: './assets/',
+                cost: '$0',
+                distance: '',
+                type: '',   
+            }
+            {
+                name: 'Atracción 2',
+                info: 'Lorem...',
+                img: './assets/',
+                cost: '$0',
+                distance: '',
+                type: '',   
+            }
+        ]
+    }
+]
