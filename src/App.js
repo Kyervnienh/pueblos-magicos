62b01a814870b04fa52704cfba5ace3e313cd825
Jesond <js.pineda.sa@gmail.com>
2021-04-18 04:30:13 -0500

    feat: login

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import './App.scss';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import SignUpPage from './components/pages/signup'
-import Home from '../src/components/pages/'
-
+import SignUpPage from './components/pages/signup';
+import Home from '../src/components/pages/';
+import Login from '../src/components/pages/login';
 
 function App() {
   return (
@@ -11,6 +11,7 @@ function App() {
       <Switch>
         <Route path="/" component={Home} exact></Route>
         <Route path="/registrate" component={SignUpPage} exact></Route>
+        <Route path="/iniciarsesion" component={Login} exact></Route>
       </Switch>
     </Router>
   );
---
+++ src/components/pages/login.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Login from '../../Login';
+import ScrollToTop from '../ScrollToTop';
+
+const LoginPage = () => {
+  return (
+    <>
+      <ScrollToTop />
+      <Login />
+    </>
+  );
+};
+
+export default LoginPage;
