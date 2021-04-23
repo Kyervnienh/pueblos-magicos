a378ead2da8468da01767001ddac4908ab7c0621
Kevin Lopez <kevin.kyervnienh@gmail.com>
2021-04-22 21:47:49 -0500

    added: page not found

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import SignUpPage from './components/pages/signup';
 import Home from '../src/components/pages/';
 import Login from '../src/components/pages/login';
+import NotFoundPage from './components/pages/notfound';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route path="/" component={Home} exact></Route>
         <Route path="/registrate" component={SignUpPage} exact></Route>
         <Route path="/iniciarsesion" component={Login} exact></Route>
+        <Route component={NotFoundPage}></Route>
       </Switch>
     </Router>
   );
---
+++ src/components/pages/notfound.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import NotFound from '../../NotFound';
+import Footer from '../Footer';
+
+const NotFoundPage = () => {
+    return(
+        <>
+        <NotFound />
+        <Footer />
+        </>
+    )
+}
+
+export default NotFoundPage
