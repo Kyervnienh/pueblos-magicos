7605c8bf13065addab578feb3ccfb775ee1c4507
Kevin Lopez <kevin.kyervnienh@gmail.com>
2021-04-27 01:39:43 -0500

    refactor: filter, slider & footer from bootstrap to react-bootstrap

--- src/components/Filter/index.js
+++ src/components/Filter/index.js
@@ -1,79 +1,77 @@
 import React from 'react';
 import { dataTows } from '../../fixtures/dataTownsExample.fixture';
-import './index.scss'
+import './index.scss';
+import { Button, Container, Form, Row } from 'react-bootstrap';
 
-function Filter(props) {
+const Filter = () => {
 
-    const [state, setState] = React.useState('');
+  const [state, setState] = React.useState('');
 
-    const towns = dataTows.filter(t => {
-        for (let k in Object.keys(t)){
-            if (Object.keys(t)[k] === 'state') return t
-        }
-    })
-
-    const handleSubmitCourse = (e) => {
-        alert("Estado seleccionado: " + state);
-        e.preventDefault();
+  const towns = dataTows.filter(t => {
+    for (let k in Object.keys(t)) {
+      if (Object.keys(t)[k] === 'state') return t
     }
+  })
 
-    const handleChangeTown = (e) => {
-        setState(e.target.value);
-    };
+  const handleSubmitCourse = (e) => {
+    alert("Estado seleccionado: " + state);
+    e.preventDefault();
+  }
 
-    const filterDropdown = towns.filter(function(result) {
-        return result.state === state;
-    });
+  const handleChangeTown = (e) => {
+    setState(e.target.value);
+  };
 
-    const getUnique = (arr, comp) => {
-        const unique = arr
-          //store the comparison values in array
-          .map(e => e[comp])
-    
-          // store the keys of the unique objects
-          .map((e, i, final) => final.indexOf(e) === i && i)
-    
-          // eliminate the dead keys & store unique objects
-          .filter(e => arr[e])
-    
-          .map(e => arr[e]);
+  const filterDropdown = towns.filter( result => {
+    return result.state === state;
+  });
 
-        return unique;
-    }
+  const getUnique = (arr, comp) => {
+    const unique = arr
+      //store the comparison values in array
+      .map(e => e[comp])
 
-    const uniqueCouse = getUnique(towns, "state");
+      // store the keys of the unique objects
+      .map((e, i, final) => final.indexOf(e) === i && i)
+
+      // eliminate the dead keys & store unique objects
+      .filter(e => arr[e])
+
+      .map(e => arr[e]);
+
+    return unique;
+  }
+
+  const uniqueState = getUnique(towns, "state");
 
 
-    return(
-        <>
-        <form 
-        className='cont container d-flex justify-content-around align-items-center'
-        onSubmit={handleSubmitCourse}>
-        
-            <select
-              value={state}
-              onChange={handleChangeTown}
-              className='form-select'
-            >
-              {uniqueCouse.map(town => (
-                <option  value={town.state} key={town.state}>
+  return (
+    <>
+      <Container className='cont'>
+        <Form>
+          <Row className='filter'>
+            <Form.Control as="select" custom className='filter-select' onChange={handleChangeTown}>
+              {uniqueState.map(town => (
+                <option value={town.state} key={town.state}>
                   {town.state}
                 </option>
               ))}
-            </select>
-        |
-          <input type="submit" value="Buscar" className='btn'/>
-        </form>
-        <div>
+            </Form.Control>
+                  |
+            <Button className="btn" type="button" onClick={handleSubmitCourse}>Buscar</Button>
+          </Row>
+        </Form>
+      </Container>
+      <div>
         {filterDropdown.map(town => (
           <div style={{ margin: "10px" }} key={town.name}>
             {town.name}
             <br />
           </div>
         ))}
-      </div>
-      </>
-    )
+      </div> 
+    </>
+  )
 }
 
-export default(Filter);
+export default Filter;
--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,18 +1,24 @@
 import React from 'react';
 import './index.scss';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
-function Footer() {
-    return(
-        <div className='footer'>
-            <div className='container-fluid social-icons my-3'>
-            <i className="fa fa-facebook icon" aria-hidden="true"></i>
-            <i className="fa fa-twitter icon"></i>
-            <i className="fa fa-instagram icon" aria-hidden="true"></i>
+const Footer = () =>
+        <Container fluid className='footer'>
+            <Row>
+                <Col>
+                    <i className="fa fa-facebook icon" aria-hidden="true"></i>
+                    <i className="fa fa-twitter icon"></i>
+                    <i className="fa fa-instagram icon" aria-hidden="true"></i>
+                </Col>
+            </Row>
+            
+            <Row>
+                <Col>
+                <p className='copy'>© Turi, 2021. Hecho con  <span> ❤ </span>  para el Mundo</p>
+                </Col>
+            </Row>
+        </Container>
 
-            </div>
-            <p className='copy'>© Turi, 2021. Hecho con  <span> ❤ </span>  para el Mundo</p>
-        </div>
-    )
-}
-
-export default(Footer);
+export default Footer;
