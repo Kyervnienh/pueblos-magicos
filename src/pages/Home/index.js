36f9fff9d15339322781ee4c8bfc0848a9e7891d
Jesond <js.pineda.sa@gmail.com>
2021-04-29 00:31:56 -0500

    refactor-introduccion-recomendaciones

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -7,27 +7,6 @@ import SuggestionsComponent from './components/Suggestions/index';
 import Slider from '../../components/Slider';
 import Filter from '../../components/Filter';
 
-import Jumbotron from 'react-bootstrap/Jumbotron';
-import Toast from 'react-bootstrap/Toast';
-import Container from 'react-bootstrap/Container';
-import Button from 'react-bootstrap/Button';
-
-const ExampleToast = ({ children }) => {
-  const [show, toggleShow] = useState(true);
-
-  return (
-    <>
-      {!show && <Button onClick={() => toggleShow(true)}>Show Toast</Button>}
-      <Toast show={show} onClose={() => toggleShow(false)}>
-        <Toast.Header>
-          <strong className="mr-auto">React-Bootstrap</strong>
-        </Toast.Header>
-        <Toast.Body>{children}</Toast.Body>
-      </Toast>
-    </>
-  );
-};
-
 const Home = () => {
   const [isOpen, setIsOpen] = useState(false);
   const toggle = () => {
@@ -41,17 +20,6 @@ const Home = () => {
       <Filter />
       <SuggestionsComponent />
       <IntroduccionRecomendaciones />
-      <Container className="p-3">
-        <Jumbotron>
-          <h1 className="header">Welcome To React-Bootstrap</h1>
-          <ExampleToast>
-            We now have Toasts
-            <span role="img" aria-label="tada">
-              🎉
-            </span>
-          </ExampleToast>
-        </Jumbotron>
-      </Container>
       <Footer />
     </>
   );
