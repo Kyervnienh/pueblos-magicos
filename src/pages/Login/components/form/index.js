9bf21663819fcb6cc175df7b89a5c104724d4458
PaulinaQuintero <paulina.mucito@gmail.com>
2021-05-02 22:24:15 -0500

    Agrego login de usuarios consumiendo de api, con fake autenticacion y guardando data en cookies, cambio navbar, agrego community y support

--- src/pages/Login/components/form/index.js
+++ src/pages/Login/components/form/index.js
@@ -1,24 +1,81 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { Link, withRouter } from 'react-router-dom';
 import './index.scss';
+import md5 from 'md5';
+import Cookies from 'universal-cookie';
+import Alert from 'react-bootstrap/Alert';
 
-const Login = () => {
+const baseURL = "http://localhost:4000/users/?";
+
+const cookies = new Cookies();
+
+function PasswordIncorrect(props) {
+  if(!props.isLoggedIn){
+  return <Alert variant="danger"><Alert.Heading>La contraseña y nombre de usuario no coinciden</Alert.Heading></Alert>
+  } else {
+    return null;
+  }
+}
+
+const Login = (props) => {
+  const [form, setForm] = useState({ username: '', password: '', isLogged: false });
+  const [isCorrect, setisCorrect] = useState(true);
+
+  const handleInputChange = async event => {
+    await setForm({
+        ...form,
+        [event.target.name] : event.target.value
+    })
+    console.log(form);
+}
+
+const logInSesion = async () => {
+  try {
+    const response = await fetch(baseURL  + new URLSearchParams({
+      username: form.username,
+      password: md5(form.password),
+  }));
+    const data = await response.json();
+    if (data.length > 0) {
+      let resp = data[0];
+      console.log(resp);
+      cookies.set('id', resp.id, {path:"/"});
+      cookies.set('email', resp.email, {path:"/"});
+      cookies.set('isLogged', resp.isLogged, {path:"/"});
+      cookies.set('name', resp.name, {path:"/"});
+      cookies.set('username', resp.username, {path:"/"});
+      setisCorrect(true);
+      props.history.push('/');
+
+    } else {
+      setisCorrect(false);
+    }
+  } catch(error) {
+    console.error(error);
+  }
+}
+
+useEffect(() => {
+  if(cookies.get('username')) {
+    props.history.push('/');
+  }
+}, []);
   return (
     <div className="Container">
       <Link className="IconSignIn" to ="/">TURI</Link>
       <div className="FormWrap">
         <div className="FormContent">
-          <form className="Form">
+          <form className="Form" >
             <h1 className="title">Inicia Sesión</h1>
             <div className="mb-3">
               <label htmlFor="exampleInputEmail1" className="form-label">
                 Usuario
               </label>
               <input
+                onChange={handleInputChange}
                 type="text"
                 className="form-control"
-                id="exampleInputEmail1"
-                aria-describedby="emailHelp"
+                name="username"
               />
             </div>
             <div className="mb-3">
@@ -26,15 +83,17 @@ const Login = () => {
                 Constraseña
               </label>
               <input
+                onChange={handleInputChange}
                 type="password"
                 className="form-control"
-                id="exampleInputPassword1"
+                name="password"
               />
             </div>
             <div className="text-center">
-              <button type="submit" className="FormButton">
+              <button onClick={logInSesion} type="button" className="FormButton">
                 Continuar
               </button>
+              <PasswordIncorrect isLoggedIn={isCorrect}/>
             </div>
             <span className="Text">¿No tienes una cuenta? <Link to="/registrate">Regístrate</Link></span>
           </form>
@@ -44,4 +103,4 @@ const Login = () => {
   );
 };
 
-export default Login;
+export default withRouter(Login);
