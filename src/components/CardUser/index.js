9111a79063962d4a418eb09146c9add05544d1e8
PaulinaQuintero <paulina.mucito@gmail.com>
2021-06-26 18:46:39 -0500

    Feature community completed

--- src/components/CardUser/index.js
+++ src/components/CardUser/index.js
@@ -1,17 +1,16 @@
 import React from "react";
 import avatar from "../../assets/icons/avatar.png";
+import Card from "react-bootstrap/Card";
+import './index.scss';
+
+const baseURL = 'http://localhost:8080';
 
 const CardUser = ({ name = "Antonio Moreno", profilePhoto = avatar }) => {
   return (
-    <Card style={{ width: "18rem" }}>
-      <Card.Img variant="top" src="holder.js/100px180" />
-      <Card.Body>
-        <Card.Title>Card Title</Card.Title>
-        <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </Card.Text>
-        <Button variant="primary">Go somewhere</Button>
+    <Card className="CardUser">
+      <Card.Title className="userNameTitle">{name}</Card.Title>
+      <Card.Body className="bodyCardUser">
+        <Card.Img className="profilePhoto" src={`${baseURL}/uploads/${profilePhoto.substring(15, profilePhoto.length)}`} alt="..." />
       </Card.Body>
     </Card>
   );
